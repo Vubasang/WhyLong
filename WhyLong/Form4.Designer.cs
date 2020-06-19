06119ec2b69193fdc0c47643dc443589709586f2
welp1 <minthourwnz@gmail.com>
2020-06-19 16:40:44 +0900

    Список сотрудников/исправленные миграции

--- WhyLong/Form4.Designer.cs
+++ WhyLong/Form4.Designer.cs
@@ -33,6 +33,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -82,12 +84,33 @@
             this.button4.TabIndex = 4;
             this.button4.Text = "Удалить";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 267);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(284, 264);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(64, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Обновить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -98,6 +121,7 @@
             this.Load += new System.EventHandler(this.Form4_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -108,5 +132,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
     }
 }
--- WhyLong/Form4.cs
+++ WhyLong/Form4.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
@@ -15,7 +17,11 @@ namespace WhyLong
     {
         public UserContext DB { get; set; }
         public Person User { get; set; }
-        
+        public static void AddPerson(UserContext db, string FirstName)
+        {
+          
+        }
+
         public Form4()
         {
             InitializeComponent();
@@ -30,10 +36,11 @@ namespace WhyLong
         {
 
         }
+        
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+           
         }
 
         private void Form4_Load(object sender, EventArgs e)
@@ -53,7 +60,21 @@ namespace WhyLong
             dataGridView1.Columns[2].HeaderText = "Имя";
             dataGridView1.Columns[3].HeaderText = "Отчество";
             dataGridView1.Columns[4].HeaderText = "Должность";
+        }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
